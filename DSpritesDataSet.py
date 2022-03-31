5e798bf00f0c866ba0843b46c3bb338f5eb11de8
Tom Bekor <tom.bekor@gmail.com>
2022-04-01 01:30:12 +0300

    DSpritesDataSet

---
+++ DSpritesDataSet.py
@@ -0,0 +1,129 @@
+import os
+import torch
+import numpy as np
+import pandas as pd
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+from skimage import io
+import subprocess
+
+
+class DSpritesDataSet(Dataset):
+    """DSprites Dataset from [1].
+    Disentanglement test Sprites dataset.Procedurally generated 2D shapes, from 6
+    disentangled latent factors. This dataset uses 6 latents, controlling the color,
+    shape, scale, rotation and position of a sprite. All possible variations of
+    the latents are present. Ordering along dimension 1 is fixed and can be mapped
+    back to the exact latent values that generated that image. Pixel outputs are
+    different. No noise added.
+    Notes
+    -----
+    - Link : https://github.com/deepmind/dsprites-dataset/
+    - hard coded metadata because issue with python 3 loading of python 2
+    Parameters
+    ----------
+    root : string
+        Root directory of dataset.
+    References
+    ----------
+    [1] Higgins, I., Matthey, L., Pal, A., Burgess, C., Glorot, X., Botvinick,
+        M., ... & Lerchner, A. (2017). beta-vae: Learning basic visual concepts
+        with a constrained variational framework. In International Conference
+        on Learning Representations.
+    """
+    urls = {"train": "https://github.com/deepmind/dsprites-dataset/blob/master/dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz?raw=true"}
+    files = {"train": "dsprite_train.npz"}
+    lat_names = ('shape', 'scale', 'orientation', 'posX', 'posY')
+    background_color = 'k'
+    lat_sizes = np.array([3, 6, 40, 32, 32])
+    img_size = (1, 64, 64)
+    lat_values = {'posX': np.array([0., 0.03225806, 0.06451613, 0.09677419, 0.12903226,
+                                    0.16129032, 0.19354839, 0.22580645, 0.25806452,
+                                    0.29032258, 0.32258065, 0.35483871, 0.38709677,
+                                    0.41935484, 0.4516129, 0.48387097, 0.51612903,
+                                    0.5483871, 0.58064516, 0.61290323, 0.64516129,
+                                    0.67741935, 0.70967742, 0.74193548, 0.77419355,
+                                    0.80645161, 0.83870968, 0.87096774, 0.90322581,
+                                    0.93548387, 0.96774194, 1.]),
+                  'posY': np.array([0., 0.03225806, 0.06451613, 0.09677419, 0.12903226,
+                                    0.16129032, 0.19354839, 0.22580645, 0.25806452,
+                                    0.29032258, 0.32258065, 0.35483871, 0.38709677,
+                                    0.41935484, 0.4516129, 0.48387097, 0.51612903,
+                                    0.5483871, 0.58064516, 0.3658, 0.96664389, 1.12775121,
+                                           1.28885852, 1.44996584, 1.61107316, 1.77218047,
+                                           1.93328779, 2.0943951, 2.25550242, 2.41660973,
+                                           2.57771705, 2.73882436, 2.89993168, 3.061039,
+                                           3.22214631, 3.38325363, 3.54436094, 3.70546826,
+                                           3.86657557, 4.02768289, 4.1887902, 4.34989752,
+                                           4.51100484, 4.67211215, 4.83321947, 4.99432678,
+                                           5.1554341, 5.31654141, 5.47764873, 5.63875604,
+                                           5.79986336, 5.96097068, 6.12207799, 6.28318531]),
+                  'shape': np.array([1., 2., 3.]),
+                  'color': np.array([1.])}
+
+    def __init__(self, root='datasets/dsprites/'):
+        super().__init__()
+
+        self.root = root
+        self.train_data = os.path.join(root, type(self).files["train"])
+
+        if not os.path.isdir(root):
+            print(("Downloading {} ...".format(str(type(self)))))
+            self.download()
+            print("Finished Downloading.")
+
+        self.transforms = transforms.Compose([transforms.ToTensor()])
+        dataset_zip = np.load(self.train_data)
+        self.imgs = dataset_zip['imgs']
+        self.lat_values = dataset_zip['latents_values']
+
+
+    def download(self):
+        """Download the dataset."""
+        os.makedirs(self.root)
+        subprocess.check_call(["curl", "-L", type(self).urls["train"],
+                               "--output", self.train_data])
+
+    def __len__(self):
+        return len(self.imgs)
+
+    def __getitem__(self, idx):
+        """Get the image of `idx`
+        Return
+        ------
+        sample : torch.Tensor
+            Tensor in [0.,1.] of shape `img_size`.
+        lat_value : np.array
+            Array of length 6, that gives the value of each factor of variation.
+        """
+        # stored image have binary and shape (H x W) so multiply by 255 to get pixel
+        # values + add dimension
+        sample = np.expand_dims(self.imgs[idx] * 255, axis=-1)
+
+        # ToTensor transforms numpy.ndarray (H x W x C) in the range
+        # [0, 255] to a torch.FloatTensor of shape (C x H x W) in the range [0.0, 1.0]
+        sample = self.transforms(sample)
+
+        lat_value = self.lat_values[idx]
+        return sample, lat_value
+
+    def images_from_data_gen(self, sample_size, y, y_lat):
+        """
+        Compute the disentanglement metric score as proposed in the original paper
+        reference: https://github.com/deepmind/dsprites-dataset/blob/master/dsprites_reloading_example.ipynb
+        """
+        #sample two sets of data generative factors such that the yth value is the same accross the two sets
+        samples = np.zeros((sample_size, self.lat_sizes.size))
+
+        for i, lat_size in enumerate(self.lat_sizes):
+            samples[:, i] = y_lat if i == y else np.random.randint(lat_size, size=sample_size) 
+        latents_bases = np.concatenate((self.lat_sizes[::-1].cumprod()[::-1][1:],
+                                np.array([1,])))
+
+        latent_indices = np.dot(samples, latents_bases).astype(int)
+    
+        #use the data generative factors to simulate two sets of images from the dataset
+        imgs_sampled = torch.from_numpy(self.imgs[latent_indices]).unsqueeze_(1).float()
+
+        #print(imgs_sampled.shape)
+        return imgs_sampled
